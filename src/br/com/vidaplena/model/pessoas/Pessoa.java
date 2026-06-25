8fd103cb04a2e3f55b043a0619b86b5540772901
eduardosyw <eduardosyw@gmail.com>
2026-06-25 10:34:04 -0300

    Create abstract class Pessoa with basic attributes

---
+++ src/br/com/vidaplena/model/pessoas/Pessoa.java
@@ -0,0 +1,47 @@
+package br.com.vidaplena.model.pessoas;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected int idade;
+    protected String telefone;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public abstract void exibirResumo();
+}
