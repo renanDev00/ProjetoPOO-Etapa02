c2129dca0a46ede630ccaf24dec3398b563c0a38
eduardosyw <eduardosyw@gmail.com>
2026-06-27 19:53:59 -0300

    Refactor setAvisoValidarCpf method for clarity

--- src/br/com/vidaplena/model/pessoas/Paciente.java
+++ src/br/com/vidaplena/model/pessoas/Paciente.java
@@ -62,7 +62,7 @@ public class Paciente extends Pessoa {
         if (cpf == null || cpf.trim().isEmpty()) {
             throw new IllegalArgumentException("O CPF nao pode ser nulo ou vazio.");
         }
-        super.setCpf(cpf); // Alteração feita aqui! Delegando para a superclasse.
+        super.setCpf(cpf);
     }
 
     public void complementar(int idade, String telefone) {
